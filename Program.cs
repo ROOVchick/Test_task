f2bbb879cd6523c8bc249808bba658bc55e14b27
Alexander Martynov <Soulroov@gmail.com>
2021-04-05 22:27:51 +0300

    test update

--- Program.cs
+++ Program.cs
@@ -6,10 +6,12 @@ namespace Test_task
     {
         static void Main(string[] args)
         {
+           
+            /*
             int money;
             Console.WriteLine("Введите сумму:");
 
-            for (money = 2700; money < 3000; money++)
+           
             {
                 Console.WriteLine($"Ваша сумма на счёте: {money} рублей");
             }
@@ -18,6 +20,8 @@ namespace Test_task
                 Console.WriteLine("Вы достигли лимита в 3000р");
             }
             Console.ReadLine();
+
+            */
         }
     }
 }
