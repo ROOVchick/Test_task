9df5d58d5110435df629e854400bcb072064b5ba
Alexander Martynov <Soulroov@gmail.com>
2021-04-05 22:25:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Test_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            Console.WriteLine("Введите сумму:");
+
+            for (money = 2700; money < 3000; money++)
+            {
+                Console.WriteLine($"Ваша сумма на счёте: {money} рублей");
+            }
+            if (money == 3000)
+            {
+                Console.WriteLine("Вы достигли лимита в 3000р");
+            }
+            Console.ReadLine();
+        }
+    }
+}
